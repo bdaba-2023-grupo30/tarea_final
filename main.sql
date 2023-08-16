58d03c7197d453366e6270e945ece01519e64784
bdaba-2023-grupo30 <142256283+bdaba-2023-grupo30@users.noreply.github.com>
2023-08-16 16:10:01 -0400

    Modificacion de archivo KSQL

--- main.sql
+++ main.sql
@@ -1,35 +1,40 @@
 CREATE STREAM stock_dato (symbol VARCHAR, price DOUBLE, volume INT, timestamp VARCHAR)
   WITH (kafka_topic='stock-pedido', value_format='json', partitions=1);
 
-CREATE TABLE pregunta_1 AS
+-- Minimo precio ponderado por simbolo de moneda
+CREATE TABLE Precio_Ponderado_Simbolo AS
     SELECT symbol,
            avg(price/volume) AS avg_pond
     FROM stock_dato
     GROUP BY symbol
     EMIT CHANGES;
 
-CREATE TABLE pregunta_2 AS
+-- Transacciones procesadas por simbolo
+CREATE TABLE Transacciones_x_Simbolo AS
     SELECT symbol,
            count(symbol) AS transac
     FROM stock_dato
     GROUP BY symbol
     EMIT CHANGES;
 
-CREATE TABLE pregunta_3 AS
+-- Maximo Precio por Simbolo
+CREATE TABLE Maximo_Precio_x_Simbolo AS
     SELECT symbol,
            max(price) AS max_price
     FROM stock_dato
     GROUP BY symbol
     EMIT CHANGES;
 
-CREATE TABLE pregunta_4 AS
+-- Minimo Precio por Simbolo
+CREATE TABLE Minimo_Precio_x_Simbolo AS
     SELECT symbol,
            min(price) AS min_price
     FROM stock_dato
     GROUP BY symbol
     EMIT CHANGES;
 
-CREATE TABLE pregunta_all AS
+-- Analisis Precio por Simbolo
+CREATE TABLE Analisis_Precio_x_Simbolo AS
     SELECT symbol,
            sum(price) AS sum_price,
            sum(volume) AS sum_volume,
@@ -39,10 +44,16 @@ CREATE TABLE pregunta_all AS
            min(price) AS min_price
     FROM stock_dato
     GROUP BY symbol
-    EMIT CHANGES;
+     EMIT CHANGES;
 
-SELECT * FROM pregunta_1;
-SELECT * FROM pregunta_2;
-SELECT * FROM pregunta_3;
-SELECT * FROM pregunta_4;
-SELECT * FROM pregunta_all;
+--Pregunta 1
+SELECT * FROM Precio_Ponderado_Simbolo;
+--Pregunta 2
+SELECT * FROM Transacciones_x_Simbolo;
+--Pregunta 3
+SELECT * FROM Maximo_Precio_x_Simbolo;
+--Pregunta 4
+SELECT * FROM Minimo_Precio_x_Simbolo;
+
+--Analisis global de las 4 preguntas
+SELECT * FROM Analisis_Precio_x_Simbolo;
